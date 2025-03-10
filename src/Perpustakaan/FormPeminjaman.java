42d4e2b6b21845aefe0f2fe9963d519bcc65764f
safirarhm <149888901+safirarhm@users.noreply.github.com>
2025-03-10 23:03:41 +0700

    Add files via upload

---
+++ src/Perpustakaan/FormPeminjaman.java
@@ -0,0 +1,12 @@
+package perpustakaan;
+
+import javax.swing.*;
+
+public class FormPeminjaman extends JFrame {
+    public FormPeminjaman() {
+        setTitle("Form Peminjaman");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+}
